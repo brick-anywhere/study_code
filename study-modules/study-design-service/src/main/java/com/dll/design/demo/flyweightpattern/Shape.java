d82500a2c78e42b1f250e12e2a114960d7e050df
dll <18435753805@163.com>
2021-05-20 14:57:06 +0800

    【设计模式】 享元模式（hashcode）

---
+++ study-modules/study-design-service/src/main/java/com/dll/design/demo/flyweightpattern/Shape.java
@@ -0,0 +1,9 @@
+package com.dll.design.demo.flyweightpattern;
+
+/**
+ * @author dll
+ * @date 2021-05-20 14:44
+ */
+public interface Shape {
+    void draw();
+}
---
+++ study-modules/study-design-service/src/main/java/com/dll/design/demo/flyweightpattern/ShapeFactory.java
@@ -0,0 +1,22 @@
+package com.dll.design.demo.flyweightpattern;
+
+import java.util.HashMap;
+
+/**
+ * @author dll
+ * @date 2021-05-20 14:45
+ */
+public class ShapeFactory {
+    private static final HashMap<String, Shape> circleMap = new HashMap<>();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle)circleMap.get(color);
+
+        if(circle == null) {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            System.out.println("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+}
